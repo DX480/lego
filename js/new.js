922c596c7b2f28b411b4ab38fa2460a56cf94331
boom_tanso <m72823062@gmail.com>
2026-01-30 11:06:03 +0900

    new 수정

--- js/new.js
+++ js/new.js
@@ -1,65 +1,55 @@
+
 var swiperA = new Swiper(".mySwiperA", {
-      direction: "vertical",
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      centeredSlidesBounds: true,
-      slidesPerView: '3',
-      autoHeight:true,
+  direction: "vertical",
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  slidesPerView: "3",
+  autoHeight: true,
 
-      touchRatio: 0,
-      speed: 1200,
-      a11y: false,
-      
-      coverflowEffect: {
+  speed: 1200,
+  a11y: false,
 
-        rotate: 50,
-        stretch: 0,
-        depth: 0,
-        modifier: 0.7,
-        slideShadows: true,
-      },
-      pagination:false ,
-      mousewheel: {
-        invert: false,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 0,
+    modifier: 0.7,
+    slideShadows: true,
+  },
+  pagination: false,
+  mousewheel: {
+    invert: false,
+  },
 
-      },
-
-      on: {
-
-        slideChange: function () {
-
-      },}
-    });
+  on: {
+    slideChange: function () {},
+  },
+});
 
 var swiperB = new Swiper(".mySwiperB", {
-      direction: "vertical",
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: '1',
-      autoHeight:true,
+  direction: "vertical",
+   effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "1",
+  
+  speed: 1500,
+  a11y: false,
 
-      touchRatio: 0,
-      speed: 1200,
-      a11y: false,
-      
-      coverflowEffect: {
+  pagination: false,
+  mousewheel: {
+    invert: false,
+  },
 
-      
-      },
-      pagination:false ,
-      mousewheel: {
-        invert: false,
-
-      },
-
-      on: {
-
-        slideChange: function () {
-
-      },}
-    });
+  on: {
+    slideChange: function () {},
+  },
+});
 
 const $contents = document.querySelector(".contents");
 
@@ -77,37 +67,48 @@ function moveBoth(dir) {
     swiperB.slidePrev();
   }
 
-
-  window.setTimeout(() => {
-    locked = false;
-  }, Math.max(swiperA.params.speed, swiperB.params.speed) + 50);
+  window.setTimeout(
+    () => {
+      locked = false;
+    },
+    Math.max(swiperA.params.speed, swiperB.params.speed) + 50,
+  );
 }
 
 //
-$contents.addEventListener("wheel", (e) => {
-  e.preventDefault();              
-  e.stopPropagation();
+$contents.addEventListener(
+  "wheel",
+  (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-  const dir = e.deltaY > 0 ? 1 : -1;
-  moveBoth(dir);
-}, { passive: false });
-
+    const dir = e.deltaY > 0 ? 1 : -1;
+    moveBoth(dir);
+  },
+  { passive: false },
+);
 
 let startY = 0;
-$contents.addEventListener("touchstart", (e) => {
-  startY = e.touches[0].clientY;
-}, { passive: true });
+$contents.addEventListener(
+  "touchstart",
+  (e) => {
+    startY = e.touches[0].clientY;
+  },
+  { passive: true },
+);
 
-$contents.addEventListener("touchmove", (e) => {
+$contents.addEventListener(
+  "touchmove",
+  (e) => {
+    e.preventDefault();
 
-  e.preventDefault();
+    const currentY = e.touches[0].clientY;
+    const diff = startY - currentY;
 
-  const currentY = e.touches[0].clientY;
-  const diff = startY - currentY;
-
- 
-  if (Math.abs(diff) > 12) {
-    moveBoth(diff > 0 ? 1 : -1);
-    startY = currentY; 
-  }
-}, { passive: false });
+    if (Math.abs(diff) > 12) {
+      moveBoth(diff > 0 ? 1 : -1);
+      startY = currentY;
+    }
+  },
+  { passive: false },
+);
